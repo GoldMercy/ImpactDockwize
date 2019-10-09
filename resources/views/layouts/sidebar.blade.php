3c4775f7b340d96355263c6511d12a640f288556
Lars van de Vrede <Lars van de Vrede>
2019-10-09 23:02:06 +0200

    Added side menu

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hide/show menu</title>
+    <style type="text/css">
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: sans-serif;
+        }
+
+        #sidebar{
+            position: fixed;
+            width: 200px;
+            height: 100%;
+            background: #151719;
+            left: -200px;
+            transition: all 500ms linear;
+        }
+
+        #sidebar.active{
+            left:0px;
+        }
+
+        #sidebar ul li{
+            color: rgba(230,230,230,0.9);
+            list-style: none;
+            padding: 15px 10px;
+            border-bottom: 1px solid rgba(100,100,100,0.3);
+        }
+
+        #sidebar .toggle-btn{
+            position: absolute;
+            left: 230px;
+            top: 20px;
+        }
+
+        #sidebar .toggle-btn span{
+            display: block;
+            width: 30px;
+            height: 5px;
+            background: #151719;
+            margin: 5px 0px;
+        }
+
+        body,html {
+            margin: 0;
+            font-family: sans-serif;
+        }
+
+        ul {
+            padding: 0;
+            width: 100%;
+        }
+
+        a {
+            outline: none;
+            text-decoration: none;
+            display: inline-block;
+            width: 100%;
+            text-align: center;
+            line-height: 3;
+            color: black;
+        }
+
+    </style>
+    <script type="text/javascript">
+        function toggleSidebar(){
+            document.getElementById("sidebar").classList.toggle('active');
+        }
+    </script>
+</head>
+<body>
+<div id="sidebar">
+    <div class="toggle-btn" onclick="toggleSidebar()"><a>
+            <span></span>
+
+            <span></span>
+
+            <span></span>
+        </a>
+    </div>
+    <ul id="buttons">
+        <li><a class href="{{ url('/home') }}">Dashboard</a></li>
+        <li><a href="{{ url('/input') }}">Input</a></li>
+        <li><a href="{{ url('/output') }}">Output</a></li>
+    </ul>
+</div>
+</body>
+</html>
