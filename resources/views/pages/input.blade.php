26b10c1f0044f67324615272e8fc7e3021d35dde
RikRodel <rdel0002@hz.nl>
2019-11-07 10:11:50 +0100

    ready to merge

--- resources/views/pages/input.blade.php
+++ resources/views/pages/input.blade.php
@@ -2,12 +2,12 @@
 @extends('layouts.sidebar')
 @section('content')
 <div class="container">
-    <a href="/openqs">
-        <button class='btn btn-primary'>Beheer uw vragen!</button>
-    </a>
-
     <a href="/cards">
         <button class='btn btn-primary'>Beheer uw fysiek ingevoerde kaartjes!</button>
+    </a>
+    
+    <a href="/openqs">
+        <button class='btn btn-primary'>Voer een open vraag in!</button>
     </a>
 
     <a href="/scaleqs">
