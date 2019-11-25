f8db327119d34f8cfa36b8720d8de23a0b77a1f7
RikRodel <rdel0002@hz.nl>
2019-11-25 14:15:09 +0100

    basic graphs for dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    {{-- {!!Charts::assets()!!} --}}
+
 </head>
 <body>
     <div id="app">
