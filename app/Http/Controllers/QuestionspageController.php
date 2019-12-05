deacfc1842072f9601945dc853fd015363462194
Pim Everse <pjeverse@gmail.com>
2019-12-05 12:28:09 +0100

    zoekfunctie vragen

--- app/Http/Controllers/QuestionspageController.php
+++ app/Http/Controllers/QuestionspageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\OpenQ;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class QuestionspageController extends Controller
@@ -12,23 +13,27 @@ class QuestionspageController extends Controller
         $this->middleware('auth');
     }
 
-/*    public function questions()
-    {
-        $data = DB::table('openqs')
-            ->leftJoin('dropdownqs', 'openqs.openq_id', '=', 'dropdownqs.dropdownq_id')
-            ->leftJoin('scaleqs', 'openqs.openq_id', '=', 'scaleqs.scaleq_id')
-            ->select('openqs.openq_id', 'openqs.openq_name', 'dropdownq_id', 'dropdownq_name', 'scaleq_id', 'scaleq_name')
-            ->get();
-        return view('pages.index', ['data' => $data]);
-    }*/
     public function questions(){
         $openqs = DB::table('openqs')->get() ;
         $dropdownqs = DB::table('dropdownqs')->get();
         $scaleqs = DB::table('scaleqs')->get();
+        $multiqs = DB::table('multiplechoice')->get();
 
-        return view('pages.index', ['openqs' => $openqs, 'dropdownqs' => $dropdownqs, 'scaleqs' => $scaleqs]);
+
+        return view('pages.index', ['openqs' => $openqs, 'dropdownqs' => $dropdownqs, 'scaleqs' => $scaleqs, 'multiqs' => $multiqs]);
 
     }
+
+    public function questionsselect(Request $request){
+        $openqs = DB::table('openqs')->where('openq_name', 'LIKE', '%' . $request->value . '%')->get();
+        $dropdownqs = DB::table('dropdownqs')->where('dropdownq_name', 'LIKE', '%' . $request->value . '%')->get();
+        $scaleqs = DB::table('scaleqs')->where('scaleq_name', 'LIKE', '%' . $request->value . '%')->get();
+        $multiqs = DB::table('multiplechoice')->where('multiplechoice_name', 'LIKE', '%' . $request->value . '%')->get();
+
+        return view('pages.index', ['openqs' => $openqs, 'dropdownqs' => $dropdownqs, 'scaleqs' => $scaleqs, 'multiqs' => $multiqs]);
+
+    }
+
     public function edit($id)
     {
         $openq = Openq::find($id);
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 @extends('layouts.sidebar')
 @section('content')
-    <div class="container">
+    <div class="container-admin">
+        <form method="get" action="/questionsselect">
+            <div class="form-row align-items-end">
+        <div class="form-group col-sm-2">
+            <label for="value">Zoeken naar:</label>
+            <input type="text" class="form-control" name="value" aria-describedby="value">
+        </div>
+        <div class="form-group col-sm-2">
+            <button type="submit" class="btn btn-primary">Zoeken</button>
+            <a href="/questions">
+                <button type="submit" class="btn btn-primary">Toon Alles</button>
+            </a>
+        </div>
+    </div>
+    </form>
         <div class="float-left">
             <table class="table table-bordered">
                 <tr>
@@ -36,6 +50,19 @@
                 @foreach($scaleqs as $sq)
                     <tr>
                         <td><a href="/scaleqs/show/{{$sq->scaleq_id}}">{{$sq->scaleq_name}}</a></td>
+                        @endforeach
+                    </tr>
+            </table>
+        </div>
+
+        <div class="float-left">
+            <table class="table table-bordered">
+                <tr>
+                    <th>Multiplechoice vragen</th>
+                </tr>
+                @foreach($multiqs as $mq)
+                    <tr>
+                        <td><a href="/multiplechoice/show/{{$mq->multiplechoice_id}}">{{$mq->multiplechoice_name}}</a></td>
                         @endforeach
                     </tr>
             </table>
